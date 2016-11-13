4009fa3e01a05b400966261558286b0864a62b34
nxycdl <nxycdl@163.com>
2016-11-13 12:19:33 +0800

    frist COmmit

---
+++ src/Helloworld.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016-11-13.
+ */
+ReactDOM.render(<h1 style={{color:'red'}}>这是通过外部js导入进来的</h1>,document.getElementById('example2'));
+ReactDOM.render(<h1 style={{color:'red'}}>style 必须是双括号，括起来， 起来的并且是json格式的color:'red'</h1>,document.getElementById('example3'));
